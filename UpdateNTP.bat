83f2c51e789bf2e965d8fc79e402168c6f03636a
Lucas H <75590817+lucasvhol@users.noreply.github.com>
2024-06-11 22:57:42 -0300

    UpdateNTP.bat fix

--- UpdateNTP.bat
+++ UpdateNTP.bat
@@ -1,13 +1,10 @@
 @echo off
-:: Verificar privilégios de administrador... / Checking admin access...
 net session >nul 2>&1
 if %errorlevel% == 1 (
-    echo Solicitando privilégios de administrador... / Requesting administrator privilege...
     PowerShell -Command "Start-Process cmd -ArgumentList '/c %~f0' -Verb RunAs"
     exit /b
 )
 
-:: Adicionar servidores NTP / Add NTP servers
 reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\DateTime\Servers" /v 1 /d "a.st1.ntp.br" /f
 reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\DateTime\Servers" /v 2 /d "b.st1.ntp.br" /f
 reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\DateTime\Servers" /v 3 /d "c.st1.ntp.br" /f
@@ -18,11 +15,9 @@ reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\DateTime\Servers" /v 7 /
 reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\DateTime\Servers" /v 8 /d "gps.ntp.br" /f
 reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\DateTime\Servers" /ve /d 1 /f
 
-:: Reiniciar o serviço de Horário do Windows / Restart Windows Time Service.
-net stop w32time && net start w32time
-
-:: Forçar atualização do horário / Force Time Refresh.
+sc config w32time start= auto
+net start w32time
 w32tm /resync
 
-echo Servidores NTP atualizados e tempo sincronizado.  / NTP servers updated and time synced.
+echo Servidores NTP atualizados e tempo sincronizado.
 pause
